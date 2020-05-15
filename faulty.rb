ca26f12fb77a6ebb5c01d496dde2cd1d8d87f680
ryoken nakayama <ryoken7090@gmail.com>
2020-05-15 20:14:48 +0900

    web技術入門課題作成

---
+++ faulty.rb
@@ -0,0 +1,14 @@
+require 'cgi'
+cgi = CGI.new
+# データを受け取った後、HTMLの形式でレスポンスを返す
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+  # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
+  # HTMLでレスポンスを返却する
+  get = cgi['faulty']
+  "<html>
+    <body>
+      <p>自家消費でないゴーヤの情報です。</p>
+      #{get}
+    </body>
+  </html>"
+}
---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => './',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+  })
+  ['INT','TERM'].each{|signal|
+    Signal.trap(signal){ server.shutdown }
+  }
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'index.html.erb')
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/give.cgi', WEBrick::HTTPServlet::CGIHandler, 'give.rb')
+server.mount('/faulty.cgi', WEBrick::HTTPServlet::CGIHandler, 'faulty.rb')
+server.start
